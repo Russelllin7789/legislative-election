6b47493f7072433be3ffd926101e36381726dd75
Russell Lin <superbr7789@gmail.com>
2023-11-14 07:14:17 +0800

    fulfill the events section

--- src/section/events/Index.vue
+++ src/section/events/Index.vue
@@ -1,12 +1,12 @@
 <template>
-  <section>
-    <div class="flex flex-col items-center justify-center py-[104px]">
+  <section class="py-[104px]">
+    <div class="flex flex-col items-center justify-center pb-[104px]">
       <div
         class="flex text-base font-bold text-[#ffffff] bg-gray-darker rounded-lg px-3 py-2"
       >
         LATEST EVENTS
       </div>
-      <h2 class="text-orange-primary py-4 text-[64px] leading-[93px]">
+      <h2 class="text-orange-primary pt-4 text-[64px] leading-[93px]">
         最新活動
       </h2>
     </div>
@@ -20,16 +20,77 @@
               alt="third event"
             />
           </div>
+          <div class="pt-4">
+            <div class="text-sm text-gray-primary">2023/12/26</div>
+            <h3 class="text-xl leading-[30px] font-bold">
+              參與台北論壇，爭取貓咪友善環境
+            </h3>
+            <p class="text-base">
+              炎炎夏日的周六，我走進了台北寵物論壇，帶著一副貓耳髮箍，決定要全力宣傳「貓咪至上」的理念！我相信，我們的都市中，每一隻貓咪都應該有自己的
+              VIP 休憩空間。
+            </p>
+          </div>
         </div>
-        <div class="flex w-[50%]">
-          <div class="w-full flex items-center">
-            <div class="w-[200px]">
+        <div class="flex flex-col w-[50%]">
+          <div class="w-full flex items-center mb-4">
+            <div class="w-[200px] min-w-[200px] mr-4">
               <img
                 src="/events-one.png"
                 class="w-full h-full object-cover"
                 alt="first event"
               />
             </div>
+            <div>
+              <div class="text-sm text-gray-primary">2023/12/24</div>
+              <h3 class="text-base font-bold">
+                掃街模式開啟！帶著你的貓耳，來和我一起走！
+              </h3>
+              <p class="text-base">
+                街上氣氛真的很棒，從小孩到大人，甚至有些狗狗朋友都帶著貓耳來找我握手，真的太可愛了！
+              </p>
+            </div>
+          </div>
+          <div class="w-full flex items-center mb-4">
+            <div class="w-[200px] min-w-[200px] mr-4">
+              <img
+                src="/events-two.png"
+                class="w-full h-full object-cover"
+                alt="second event"
+              />
+            </div>
+            <div>
+              <div class="text-sm text-gray-primary">2023/12/20</div>
+              <h3 class="text-base font-bold">收容所模特兒大比拼！</h3>
+              <p class="text-base">
+                今天的收容所不再是一片寂靜。為了讓更多人認識到這裡的毛孩子，我們舉辦了一場前所未有的「模特兒走秀」！
+              </p>
+            </div>
+          </div>
+          <div class="w-full flex items-center mb-4">
+            <div class="w-[200px] min-w-[200px] mr-4">
+              <img
+                src="/events-three.png"
+                class="w-full h-full object-cover"
+                alt="third event"
+              />
+            </div>
+            <div>
+              <div class="text-sm text-gray-primary">2023/12/26</div>
+              <h3 class="text-base font-bold">
+                參與台北論壇，爭取貓咪友善環境
+              </h3>
+              <p class="text-base">
+                炎炎夏日的周六，我走進了台北寵物論壇，帶著一副貓耳髮箍，決定要全力宣傳「貓咪至上」的理念！我相信，我們的都市中，每一隻貓咪都應該有自己的
+                VIP 休憩空間。
+              </p>
+            </div>
+          </div>
+          <div>
+            <button
+              class="py-4 px-6 bg-gray-lighter text-base leading-[20px] font-[600] rounded-[500px]"
+            >
+              查看更多
+            </button>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ export default {
       "orange-primary": "#DA7D4A",
       "orange-light": "#F7ECE1",
       "gray-darker": "#334155",
+      "gray-primary": "#94A3B8",
+      "gray-lighter": "#F1F5F9",
       "blue-light": "#E8E3FE",
     },
   },
