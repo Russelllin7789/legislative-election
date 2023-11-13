afe7bcc4d01c7aacad881c1a72b205d34f7bf95d
Russell Lin <superbr7789@gmail.com>
2023-11-13 22:04:35 +0800

    add photos section

--- src/section/events/Index.vue
+++ src/section/events/Index.vue
@@ -10,6 +10,29 @@
         最新活動
       </h2>
     </div>
-    <div class="w-[1200px] flex items-center justify-center"></div>
+    <div class="w-full flex items-center justify-center">
+      <div class="w-[1200px] flex items-start">
+        <div class="flex flex-col w-[50%] mr-6">
+          <div class="w-full rounded-2xl overflow-hidden">
+            <img
+              src="/events-three.png"
+              class="w-full h-full object-cover"
+              alt="third event"
+            />
+          </div>
+        </div>
+        <div class="flex w-[50%]">
+          <div class="w-full flex items-center">
+            <div class="w-[200px]">
+              <img
+                src="/events-one.png"
+                class="w-full h-full object-cover"
+                alt="first event"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
